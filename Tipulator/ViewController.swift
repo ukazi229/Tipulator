4dd3f580fd38e322406eafb1b44d5aa61d92f2f6
rainarit <rraina@ucsd.edu>
2020-01-25 09:59:30 -0800

    Initial Commit!

---
+++ Tipulator/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  Tipulator
+//
+//  Created by Umar Kazi on 1/23/20.
+//  Copyright © 2020 Umar Kazi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func onTap(_ sender: Any) {
+        print("Hello")
+        
+        view.endEditing(true)
+    
+    }
+    
+    @IBAction func calculateTip(_ sender: Any) {
+        
+        // Get the bill amount
+        let bill = Double(billField.text!) ?? 0
+        
+        // Calculate the tip and total
+        let tipPercentages = [0.15, 0.18, 0.2]
+        
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        // Update the tip and total labels
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
+}
